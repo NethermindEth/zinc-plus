4eff25215c33b1a293893f0dcbcee1291fc50576
Ilia Vlasov <elijah.vlasov@gmail.com>
2026-01-16 12:25:04 +0000

    Update utils/src/field/const_monty.rs
    Co-authored-by: Alex Abdugafarov <frozenspider@users.noreply.github.com>

--- utils/src/field/const_monty.rs
+++ utils/src/field/const_monty.rs
@@ -86,7 +86,7 @@ mod tests {
         let sampled = F::from(5_u64);
 
         let projection_fn =
-            <Int<{ U128::LIMBS }> as ProjectableToField<F>>::prepare_projection(&sampled);
+            Int::<{ U128::LIMBS }>::prepare_projection(&sampled);
 
         let int_value = Int::<{ U128::LIMBS }>::from(10_i64);
         let result = projection_fn(&int_value);
