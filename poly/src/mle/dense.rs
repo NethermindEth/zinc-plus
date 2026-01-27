a414c16dc187464a7b58d2bc413e8e9c1b1f24eb
Alexander Abdugafarov <alexander.abdugafarov@nethermind.io>
2026-01-28 02:36:49 +0500

    Fix test broken by is_zero change

--- poly/src/mle/dense.rs
+++ poly/src/mle/dense.rs
@@ -621,7 +621,7 @@ mod tests {
         assert!(dense.iter().enumerate().all(|(i, v)| if i == 0 || i == 5 {
             true
         } else {
-            v.is_zero_with_cfg(&cfg)
+            F::is_zero(v)
         }));
     }
 
