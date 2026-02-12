ecdffe99111f26440bf5911d8ac4f5329fa07853
Ilia Vlasov <elijah.vlasov@gmail.com>
2026-02-12 12:58:44 +0000

    WIP

--- piop/src/ideal_check/structs.rs
+++ piop/src/ideal_check/structs.rs
@@ -1,38 +1,24 @@
 use crypto_primitives::{Field, FromWithConfig, PrimeField, Semiring};
 use std::collections::HashMap;
 use zinc_poly::{
-    CoefficientProjectable, mle::DenseMultilinearExtension,
-    univariate::dynamic::over_field::DynamicPolynomialF,
+    mle::DenseMultilinearExtension, univariate::dynamic::over_field::DynamicPolynomialF,
 };
 use zinc_transcript::traits::ConstTranscribable;
 use zinc_utils::{
     inner_transparent_field::InnerTransparentField, projectable_to_field::ProjectableToField,
 };
 
-pub trait IdealCheckTypes<const DEGREE_PLUS_ONE: usize> {
-    type WitnessCoeff;
-    type Witness: Semiring
-        + CoefficientProjectable<Self::WitnessCoeff, DEGREE_PLUS_ONE>
-        + ProjectableToField<Self::F>
-        + ConstTranscribable
-        + Send
-        + Sync
-        + 'static;
-
-    type F: InnerTransparentField + FromWithConfig<Self::WitnessCoeff> + Send + Sync + 'static;
+#[derive(Clone, Debug)]
+pub struct Proof<F: PrimeField> {
+    pub combined_mle_values: Vec<DynamicPolynomialF<F>>,
 }
 
 #[derive(Clone, Debug)]
-pub struct Proof<IcTypes: IdealCheckTypes<DEGREE_PLUS_ONE>, const DEGREE_PLUS_ONE: usize> {
-    pub combined_mle_values: Vec<DynamicPolynomialF<IcTypes::F>>,
-}
-
-#[derive(Clone, Debug)]
-pub struct ProverState<IcTypes: IdealCheckTypes<DEGREE_PLUS_ONE>, const DEGREE_PLUS_ONE: usize> {
-    pub evaluation_point: Vec<IcTypes::F>,
-    pub combined_mles: Vec<Vec<DenseMultilinearExtension<<IcTypes::F as Field>::Inner>>>,
-    pub trace_matrix: Vec<DenseMultilinearExtension<DynamicPolynomialF<IcTypes::F>>>,
-    pub projected_scalars: HashMap<IcTypes::Witness, DynamicPolynomialF<IcTypes::F>>,
+pub struct ProverState<R: Semiring, F: PrimeField> {
+    pub evaluation_point: Vec<F>,
+    pub combined_mles: Vec<Vec<DenseMultilinearExtension<F::Inner>>>,
+    pub trace_matrix: Vec<DenseMultilinearExtension<DynamicPolynomialF<F>>>,
+    pub projected_scalars: HashMap<R, DynamicPolynomialF<F>>,
 }
 
 pub struct VerifierSubClaim<R: Semiring, F: PrimeField> {
